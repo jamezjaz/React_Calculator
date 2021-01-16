f48f52c09d221fa3b41f8e764abe1e1e5eeea29e
jamezjaz <jamezjaz@gmail.com>
2021-01-16 11:48:23 -0600

    docs: default value of result prop set to 0

--- src/components/Display.js
+++ src/components/Display.js
@@ -15,7 +15,11 @@ const Display = props => {
 };
 
 Display.propTypes = {
-  result: PropTypes.string.isRequired,
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: 0,
 };
 
 export default Display;
