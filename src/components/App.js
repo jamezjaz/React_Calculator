30921a915d01544fa847719b65fc50c0d88fe9f4
jamezjaz <jamezjaz@gmail.com>
2021-01-19 10:33:00 -0600

    docs: Made App component a stateful class component and intialized total, next & operation

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,35 @@
+/* eslint-disable */
+
+import React from 'react';
 import '../styles/App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <div className="App">
-        <header className="App-header">
-          <h3>REACT CALCULATOR</h3>
-        </header>
-        <Display result="5 * 5" />
-        <ButtonPanel />
-      </div>
-    </>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <div className="App">
+          <header className="App-header">
+            <h3>REACT CALCULATOR</h3>
+          </header>
+          <Display result="5 * 5" />
+          <ButtonPanel />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
