81bb89047522e1c520dd9c9c08ef388828c2fbf8
jamezjaz <jamezjaz@gmail.com>
2021-01-19 15:56:43 -0600

    docs: Refactors ButtonPanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,38 +2,49 @@ import React from 'react';
 import Button from './Button';
 import '../styles/ButtonPanel.css';
 
-const ButtonPanel = () => (
-  <div>
-    <div className="calculator-grid">
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button type="button">÷</button>
+const ButtonPanel = () => {
+  const buttonGroups = {
+    group1: {
+      id: 1,
+      group: ['AC', '+/-', '%', '÷'],
+    },
+    group2: {
+      id: 2,
+      group: ['7', '8', '9', 'X'],
+    },
+    group3: {
+      id: 3,
+      group: ['4', '5', '6', '-'],
+    },
+    group4: {
+      id: 4,
+      group: ['1', '2', '3', '+'],
+    },
+    group5: {
+      id: 5,
+      group: ['0', '.', '='],
+    },
+  };
+
+  return (
+    <div>
+      <div className="calculator-grid">
+        {buttonGroups.group1.group.map(btn => (<Button name={btn} key={btn} />))}
+      </div>
+      <div className="calculator-grid">
+        {buttonGroups.group2.group.map(btn => (<Button name={btn} key={btn} />))}
+      </div>
+      <div className="calculator-grid">
+        {buttonGroups.group3.group.map(btn => (<Button name={btn} key={btn} />))}
+      </div>
+      <div className="calculator-grid">
+        {buttonGroups.group4.group.map(btn => (<Button name={btn} key={btn} />))}
+      </div>
+      <div className="calculator-grid">
+        {buttonGroups.group5.group.map(btn => (<Button name={btn} key={btn} className={btn === '0' ? 'span-two' : null} />))}
+      </div>
     </div>
-    <div className="calculator-grid">
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button">X</button>
-    </div>
-    <div className="calculator-grid">
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button">-</button>
-    </div>
-    <div className="calculator-grid">
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button type="button">+</button>
-    </div>
-    <div className="calculator-grid">
-      <button type="button" className="span-two">0</button>
-      <button type="button">.</button>
-      <button type="button">=</button>
-    </div>
-    <Button name="Calculator" />
-  </div>
-);
+  );
+};
+
 export default ButtonPanel;
