cd65944865a35571e7f46888e7b6ec88edf39565
jamezjaz <jamezjaz@gmail.com>
2021-01-29 14:12:05 -0600

    docs: Links the App (calculator) component using React Router

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './components/Home';
+import App from './components/App';
 
 const Routes = () => (
   <Router>
     <Nav />
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/calculator" component={App} />
     </Switch>
   </Router>
 );
