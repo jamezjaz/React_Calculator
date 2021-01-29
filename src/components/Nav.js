f2af92934696674a9fd9a9e47e3d30a17a1b39e7
jamezjaz <jamezjaz@gmail.com>
2021-01-29 14:24:44 -0600

    docs: Add page links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import '../styles/Nav.css';
+import { Link } from 'react-router-dom';
 
 const Nav = () => (
   <nav>
     <h2>Math Magicians</h2>
     <ul className="nav-links">
-      <li>Home</li>
-      <li>Calculator</li>
-      <li>Quotes</li>
+      <Link to="/">
+        <li>Home</li>
+      </Link>
+      <Link to="/calculator">
+        <li>Calculator</li>
+      </Link>
+      <Link to="/quotes">
+        <li>Quotes</li>
+      </Link>
     </ul>
   </nav>
 );
