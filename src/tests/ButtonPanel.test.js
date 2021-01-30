177fb4c6889ddecd7536e23d146549d9458670a9
jamezjaz <jamezjaz@gmail.com>
2021-01-30 16:49:37 -0600

    test: Add more tests on ButtonPanel.test.js file

--- src/tests/ButtonPanel.test.js
+++ src/tests/ButtonPanel.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+ 
+Enzyme.configure({ adapter: new Adapter() });
 import ButtonPanel from '../components/ButtonPanel';
+import Button from '../components/Button';
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<ButtonPanel />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+describe('<ButtonPanel />', () => {
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<ButtonPanel />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders a total of 19 buttons', () => {
+    const buttonPanel = shallow(<ButtonPanel />);
+    expect(buttonPanel.find(Button)).toHaveLength(19);
+  });
 });
