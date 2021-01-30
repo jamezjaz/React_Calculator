d20495810d4a0df5b74cc6845db6b710d7106449
jamezjaz <jamezjaz@gmail.com>
2021-01-30 12:15:53 -0600

    test: Adds snapshot test for Diplay component

---
+++ src/tests/Display.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Display from '../components/Display';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Display />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
