0f4bdc7c54481f8475eca0f278117e1551f8957f
jamezjaz <jamezjaz@gmail.com>
2021-01-30 16:37:11 -0600

    test: Update Button component tests

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import Button from '../components/Button';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+ 
+Enzyme.configure({ adapter: new Adapter() });
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Button />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+describe('<Button />', () => {
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Button />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('it contains a button', () => {
+    let wrapper = shallow(<Button />);
+    const button = <button></button>;
+    expect(wrapper.containsMatchingElement(button)).toBe(true);
+  });
+
+  it('renders the button texts correctly', () => {
+    const { getByText } = render(<Button name="÷" />);
+    const divButton = getByText(/÷/i);
+    expect(divButton).toBeInTheDocument();
+  });
+
+  it('the buttons play the role of buttons', () => {
+    const { getByRole } = render(<Button name="÷" />);
+    const divButton = getByRole('button');
+    expect(divButton).toBeInTheDocument();
+  });
 });
