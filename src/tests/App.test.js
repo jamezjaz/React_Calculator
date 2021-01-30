654571a87772c4caaaec4abe7dbfabf94cc9d901
jamezjaz <jamezjaz@gmail.com>
2021-01-30 15:27:20 -0600

    test: Update tests on App.test.js file

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+ 
+Enzyme.configure({ adapter: new Adapter() });
 import App from '../components/App';
+import Display from '../components/Display';
+import ButtonPanel from '../components/ButtonPanel';
 
 it('it renders correctly', () => {
   const tree = renderer
     .create(<App />)
     .toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+describe('<App />', () => {
+  it('renders Display correctly', () => {
+    let wrapper = shallow(<App />);
+    expect(wrapper.find(Display)).toHaveLength(1);
+  });
+});
+
+describe('<App />', () => {
+  it('renders ButtonPanel correctly', () => {
+    let wrapper = shallow(<App />);
+    expect(wrapper.find(ButtonPanel)).toHaveLength(1);
+  });
 });
