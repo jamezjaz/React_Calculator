c1c92a57c3cbe5bb4103e79959f6707d18ed812f
jamezjaz <jamezjaz@gmail.com>
2021-01-31 07:54:27 -0600

    test: Add more tests to Quotes component

--- src/tests/Quotes.test.js
+++ src/tests/Quotes.test.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+ 
+Enzyme.configure({ adapter: new Adapter() });
 import Quotes from '../components/Quotes';
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Quotes />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+describe('<Quotes />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Quotes />);
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Quotes />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
+  it('it contains <strong> element', () => {
+    const strong = <strong>— Albert Einstein</strong>;
+    expect(wrapper.containsMatchingElement(strong)).toBe(true);
+  });
+
+  it('<div> element has a <p> element', () => {
+    const div = wrapper.find('.quotes');
+    const para = 
+      <p>
+        * Pure mathematics is, in its way, the poetry of logical ideas.
+        <strong> — Albert Einstein</strong>
+      </p>
+    expect(div.containsMatchingElement(para)).toBe(true);
+  });
+
+  it('should fail if the text on <strong> element is incorrect', () => {
+    const strongText = <strong> — Odufu James Chigozie</strong>;
+    expect(wrapper.containsMatchingElement(strongText)).toBe(false);
+  });
 });
