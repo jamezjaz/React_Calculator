5865b51aeeec428f1d1c0c2dc7b6c7d1584e261b
jamezjaz <jamezjaz@gmail.com>
2021-01-31 07:02:34 -0600

    test: Improves Home component tests to maintain DRY rule

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -9,6 +9,12 @@ Enzyme.configure({ adapter: new Adapter() });
 import Home from '../components/Home';
 
 describe('<Home />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Home />);
+  });
+
   it('renders correctly', () => {
     const tree = renderer
       .create(<Home />)
@@ -17,19 +23,16 @@ describe('<Home />', () => {
   });
   
   it('it contains a h2 element', () => {
-    let wrapper = shallow(<Home />);
     const h2 = <h2>Hello there!</h2>;
     expect(wrapper.containsMatchingElement(h2)).toBe(true);
   });
 
   it('h2 element has a text', () => {
-    let wrapper = shallow(<Home />);
     const h2 = wrapper.find('.heading');
     expect(h2.text()).toEqual('Hello there!');
   });
 
   it('should fail if the text on the h2 element is incorrect', () => {
-    let wrapper = shallow(<Home />);
     const h2Text = <h2>Hey there!</h2>;
     expect(wrapper.containsMatchingElement(h2Text)).toBe(false);
   });
